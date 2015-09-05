0991475165cffe945e211923bfd17990d563b120
daniel bannert <d.bannert@anolilab.de>
2015-09-05 21:44:54 +0200

    added json2scss to all css can i use data | moved some functions start adding hacks and tests

--- tasks/json_to_sass.js
+++ tasks/json_to_sass.js
@@ -2,6 +2,366 @@
 //json_to_sass config
 
 module.exports = {
+  "feature-border-radius": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/border-radius.json',
+        dest: 'src/scss/can-i-use/features/border-radius.scss'
+      }
+    ]
+  },
+  "feature-css-boxshadow": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-boxshadow.json',
+        dest: 'src/scss/can-i-use/features/css-boxshadow.scss'
+      }
+    ]
+  },
+  "feature-css-animation": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-animation.json',
+        dest: 'src/scss/can-i-use/features/css-animation.scss'
+      }
+    ]
+  },
+  "feature-css-transitions": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-transitions.json',
+        dest: 'src/scss/can-i-use/features/css-transitions.scss'
+      }
+    ]
+  },
+  "feature-transforms2d": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/transforms2d.json',
+        dest: 'src/scss/can-i-use/features/transforms2d.scss'
+      }
+    ]
+  },
+  "feature-transforms3d": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/transforms3d.json',
+        dest: 'src/scss/can-i-use/features/transforms3d.scss'
+      }
+    ]
+  },
+  "feature-css-gradients": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-gradients.json',
+        dest: 'src/scss/can-i-use/features/css-gradients.scss'
+      }
+    ]
+  },
+  "feature-css3-boxsizing": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css3-boxsizing.json',
+        dest: 'src/scss/can-i-use/features/css3-boxsizing.scss'
+      }
+    ]
+  },
+  "feature-css-filters": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-filters.json',
+        dest: 'src/scss/can-i-use/features/css-filters.scss'
+      }
+    ]
+  },
+  "feature-css-element-function": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-element-function.json',
+        dest: 'src/scss/can-i-use/features/css-element-function.scss'
+      }
+    ]
+  },
+  "feature-multicolumn": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/multicolumn.json',
+        dest: 'src/scss/can-i-use/features/multicolumn.scss'
+      }
+    ]
+  },
+  "feature-user-select-none": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/user-select-none.json',
+        dest: 'src/scss/can-i-use/features/user-select-none.scss'
+      }
+    ]
+  },
+  "feature-flexbox": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/flexbox.json',
+        dest: 'src/scss/can-i-use/features/flexbox.scss'
+      }
+    ]
+  },
+  "feature-calc": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/calc.json',
+        dest: 'src/scss/can-i-use/features/calc.scss'
+      }
+    ]
+  },
+  "feature-background-img-opts": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/background-img-opts.json',
+        dest: 'src/scss/can-i-use/features/background-img-opts.scss'
+      }
+    ]
+  },
+  "feature-font-feature": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/font-feature.json',
+        dest: 'src/scss/can-i-use/features/font-feature.scss'
+      }
+    ]
+  },
+  "feature-border-image": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/border-image.json',
+        dest: 'src/scss/can-i-use/features/border-image.scss'
+      }
+    ]
+  },
+  "feature-css-selection": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-selection.json',
+        dest: 'src/scss/can-i-use/features/css-selection.scss'
+      }
+    ]
+  },
+  "feature-css-placeholder": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-placeholder.json',
+        dest: 'src/scss/can-i-use/features/css-placeholder.scss'
+      }
+    ]
+  },
+  "feature-css-hyphens": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-hyphens.json',
+        dest: 'src/scss/can-i-use/features/css-hyphens.scss'
+      }
+    ]
+  },
+  "feature-fullscreen": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/fullscreen.json',
+        dest: 'src/scss/can-i-use/features/fullscreen.scss'
+      }
+    ]
+  },
+  "feature-css3-tabsize": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css3-tabsize.json',
+        dest: 'src/scss/can-i-use/features/css3-tabsize.scss'
+      }
+    ]
+  },
+  "feature-intrinsic-width": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/intrinsic-width.json',
+        dest: 'src/scss/can-i-use/features/intrinsic-width.scss'
+      }
+    ]
+  },
+  "feature-css3-cursors-newer": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css3-cursors-newer.json',
+        dest: 'src/scss/can-i-use/features/css3-cursors-newer.scss'
+      }
+    ]
+  },
+  "feature-css-sticky": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-sticky.json',
+        dest: 'src/scss/can-i-use/features/css-sticky.scss'
+      }
+    ]
+  },
+  "feature-pointer": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/pointer.json',
+        dest: 'src/scss/can-i-use/features/pointer.scss'
+      }
+    ]
+  },
+  "feature-text-decoration": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/text-decoration.json',
+        dest: 'src/scss/can-i-use/features/text-decoration.scss'
+      }
+    ]
+  },
+  "feature-text-size-adjust": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/text-size-adjust.json',
+        dest: 'src/scss/can-i-use/features/text-size-adjust.scss'
+      }
+    ]
+  },
+  "feature-css-masks": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-masks.json',
+        dest: 'src/scss/can-i-use/features/css-masks.scss'
+      }
+    ]
+  },
+  "feature-css-boxdecorationbreak": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-boxdecorationbreak.json',
+        dest: 'src/scss/can-i-use/features/css-boxdecorationbreak.scss'
+      }
+    ]
+  },
+  "feature-object-fit": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/object-fit.json',
+        dest: 'src/scss/can-i-use/features/object-fit.scss'
+      }
+    ]
+  },
+  "feature-css-shapes": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-shapes.json',
+        dest: 'src/scss/can-i-use/features/css-shapes.scss'
+      }
+    ]
+  },
+  "feature-text-overflow": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/text-overflow.json',
+        dest: 'src/scss/can-i-use/features/text-overflow.scss'
+      }
+    ]
+  },
+  "feature-text-emphasis": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/text-emphasis.json',
+        dest: 'src/scss/can-i-use/features/text-emphasis.scss'
+      }
+    ]
+  },
+  "feature-css-deviceadaptation": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-deviceadaptation.json',
+        dest: 'src/scss/can-i-use/features/css-deviceadaptation.scss'
+      }
+    ]
+  },
+  "feature-css-media-resolution": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-media-resolution.json',
+        dest: 'src/scss/can-i-use/features/css-media-resolution.scss'
+      }
+    ]
+  },
+  "feature-css-text-align-last": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-text-align-last.json',
+        dest: 'src/scss/can-i-use/features/css-text-align-last.scss'
+      }
+    ]
+  },
+  "feature-css-crisp-edges": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-crisp-edges.json',
+        dest: 'src/scss/can-i-use/features/css-crisp-edges.scss'
+      }
+    ]
+  },
+  "feature-css-logical-props": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-logical-props.json',
+        dest: 'src/scss/can-i-use/features/css-logical-props.scss'
+      }
+    ]
+  },
+  "feature-css-appearance": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-appearance.json',
+        dest: 'src/scss/can-i-use/features/css-appearance.scss'
+      }
+    ]
+  },
+  "feature-css-snappoints": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-snappoints.json',
+        dest: 'src/scss/can-i-use/features/css-snappoints.scss'
+      }
+    ]
+  },
+  "feature-css-regions": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-regions.json',
+        dest: 'src/scss/can-i-use/features/css-regions.scss'
+      }
+    ]
+  },
+  "feature-css-image-set": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-image-set.json',
+        dest: 'src/scss/can-i-use/features/css-image-set.scss'
+      }
+    ]
+  },
+  "feature-css-writing-mode": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-writing-mode.json',
+        dest: 'src/scss/can-i-use/features/css-writing-mode.scss'
+      }
+    ]
+  },
+  "feature-css-cross-fade": {
+    files: [
+      {
+        src: 'node_modules/caniuse-db/features-json/css-cross-fade.json.json',
+        dest: 'src/scss/can-i-use/features/css-cross-fade.json.scss'
+      }
+    ]
+  },
   data: {
     files: [
       {
